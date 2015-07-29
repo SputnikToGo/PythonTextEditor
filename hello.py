c1b8329ba288440b3f343bcfaafa51cca4afd9cc
SputnikToGo <mmlase@utu.fi>
2015-07-29 18:21:24 +0300

    Filemenun muutokset
    Pystytään avaamaan ja sulkemaan tiedostoja. Tallentaminen ei vielä
    toiminnallinen koska vammanen koodata. Stay tuned!

--- hello.py
+++ hello.py
@@ -4,44 +4,51 @@ from tkinter import Menu
 import tkinter.scrolledtext as tkst
 from tkinter import filedialog
 from tkinter import messagebox
-
-#Tässä kohtaa luodaan tekstieditori
-root = tk.Tk(className= "Tekstieditori")
-textPad = tkst.ScrolledText(root, width=100, height=50)
+from tkinter.filedialog import asksaveasfile
 
 #Aloitetaan komentojen tekeminen
-def open_command():
+
+def openCommand():
     file = tk.filedialog.askopenfile(parent=root,mode='rb', title='Valitse tiedosto')
     if file !=None:
         contents = file.read()
         textPad.insert('1.0',contents)
         file.close()
 
-def save_command(self):
-    file = tk.filedialog.asksaveasfile(mode='W')
-    if file != None:
-        data = self.textPad.get('1,0', END+'-1c')
-        file.write(data)
-        file.close()
+#def saveCommand(self):
 
 
-def exit_command():
+def exitCommand():
     if tk.messagebox.askokcancel("Poistu", "Haluatko todella poistua?"):
         root.destroy()
 #Lopetetaan komentojen tekeminen
 
+
+
+#Tässä kohtaa luodaan tekstieditori
+root = tk.Tk(className= "Tekstieditori")
+textPad = tkst.ScrolledText(root, width=100, height=25)
+
+textPad.bind()
+
 # Luodaan valikkorivi nimeltä Menu
 menu = Menu(root)
 root.config(menu=menu)
+
+
 filemenu =  Menu(menu)
 menu.add_cascade(label="Tiedosto", menu=filemenu)
-filemenu.add_command(label="Uusi")
-filemenu.add_command(label="Avaa..", command=open_command())
+filemenu.add_command(label="Avaa..", command=openCommand)
 filemenu.add_command(label="Tallenna")
 filemenu.add_separator()
-filemenu.add_command(label="Poistu", command = exit_command())
+filemenu.add_command(label="Poistu", command = exitCommand)
 #Lopetetaan valikkorivin luominen
+
 
 
 textPad.pack()
 root.mainloop()
+
+
+
+
