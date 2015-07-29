6dee42805dba22e65e0b5c7de18d99fd6645b948
SputnikToGo <mmlase@utu.fi>
2015-07-29 14:13:01 +0300

    Hevonen
    Näyttää vähän kivemmalta

--- hello.py
+++ hello.py
@@ -5,58 +5,43 @@ import tkinter.scrolledtext as tkst
 from tkinter import filedialog
 from tkinter import messagebox
 
-win = tk.Tk()
-win.title("Text editor")
-frame1 = tk.Frame(
-    master = win,
-#    bg = '#808000'
-)
+#Tässä kohtaa luodaan tekstieditori
+root = tk.Tk(className= "Tekstieditori")
+textPad = tkst.ScrolledText(root, width=100, height=50)
 
-# Luodaan tekstikenttä
-frame1.pack(fill='both', expand='yes')
-editArea = tkst.ScrolledText(
-    master = frame1,
-    wrap   = tk.WORD,
-    width  = 100,
-    height = 25
-)
-
-#Tehdään valikkoriville komennot
+#Aloitetaan komentojen tekeminen
 def open_command():
-        file = filedialog.askopenfile(parent=win, mode='rb', title='Select a file')
-        if file != None:
-            contents = file.read()
-            editArea.insert('1.0', contents)
-            file.close()
+    file = tk.filedialog.askopenfile(parent=root,mode='rb', title='Valitse tiedosto')
+    if file !=None:
+        contents = file.read()
+        textPad.insert('1.0',contents)
+        file.close()
 
-#def save_command(self):
-#        file = filedialog.asksaveasfile(mode='w')
-#        if file != None:
-#            data = self.editArea.get('1,0', END+ '-1c')
-#            file.write(data)
-#            file.close()
+def save_command(self):
+    file = tk.filedialog.asksaveasfile(mode='W')
+    if file != None:
+        data = self.textPad.get('1,0', END+'-1c')
+        file.write(data)
+        file.close()
+
 
 def exit_command():
-    if messagebox.askokcancel("Quit", "Do you really want to guit?"):
-        win.destroy()
+    if tk.messagebox.askokcancel("Poistu", "Haluatko todella poistua?"):
+        root.destroy()
 #Lopetetaan komentojen tekeminen
 
 # Luodaan valikkorivi nimeltä Menu
-menu = Menu(win)
-win.config(menu=menu)
+menu = Menu(root)
+root.config(menu=menu)
 filemenu =  Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New")
-filemenu.add_command(label="Open...", command=open_command())
-filemenu.add_command(label="Save")
+menu.add_cascade(label="Tiedosto", menu=filemenu)
+filemenu.add_command(label="Uusi")
+filemenu.add_command(label="Avaa..", command=open_command())
+filemenu.add_command(label="Tallenna")
 filemenu.add_separator()
-filemenu.add_command(label="Exit", command = exit_command())
+filemenu.add_command(label="Poistu", command = exit_command())
 #Lopetetaan valikkorivin luominen
 
-editArea.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-editArea.insert(tk.INSERT,
-"""\
-""")
 
-editArea.pack()
-win.mainloop()
+textPad.pack()
+root.mainloop()
