eb60b6d7b2cfbb8c6815b119a94a10732431949c
Jani Anttonen <JaniAnttonen@gmail.com>
2016-08-26 13:37:37 +0300

    remove test file

--- test.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Jani'
-import fileSystem
-
-file = fileSystem.File('ebinsings.txt')
-file.tag("raps",[5,4])
-file.tag("rapsings",[5,4])
-file.remove_tag("raps",[5,4])
-print(file.get_tags_by_index([5,4]))
